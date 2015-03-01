c48d10e91ffe67ca002d3c47da28ce4fff578edd
jessefurmanek <jesse.furmanek@gmail.com>
2015-03-01 13:09:43 -0600

    Add angular elements from exercise

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -11,7 +11,31 @@ app.controller('MainController', ['$scope', function($scope) {
       name: 'Mozzarella Sticks',
       description: 'Served with marinara sauce.',
       price: 3.95
+    },
+     {
+      name: 'Bruschetta',
+      description: 'Grilled bread garlic, tomatoes, olive oil',
+      price: 4.95
     }
   ];
+
+  $scope.extras = [
+    {
+      name: 'Pop',
+      description: 'now with bubbles!',
+      price: 4.95
+    },
+    {
+      name: 'Machiato',
+      description: 'now with milk!',
+      price: 3.95
+    },
+     {
+      name: 'Eggs',
+      description: 'straight from a dinosaurs toosh!',
+      price: 4.95
+    }
+
+  ]
 
 }]);
